b34fa92bdcc119bf0f11b6f871cd7a397d832232
skyming <skyming1126@gmail.com>
2021-05-16 23:48:54 +0800

    week02

--- java-advanced-project/src/main/java/com/sky/week02nio/HttpServer01.java
+++ java-advanced-project/src/main/java/com/sky/week02nio/HttpServer01.java
@@ -1,4 +1,24 @@
 package com.sky.week02nio;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class HttpServer01 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void service(Socket socket){
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
