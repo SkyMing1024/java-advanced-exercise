74f48f12cb08059ffd13cfb88177f0e658e9de1e
skyming <skyming1126@gmail.com>
2021-05-23 23:33:48 +0800

    work03

--- java-advanced-project/src/main/java/com/sky/week03nio/HttpServerInboundHandler.java
+++ java-advanced-project/src/main/java/com/sky/week03nio/HttpServerInboundHandler.java
@@ -1,4 +1,62 @@
 package com.sky.week03nio;
 
-public class HttpServerInboundHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
+
+    private ByteBuf reader;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        if (msg instanceof HttpRequest) {
+//            HttpRequest request = (HttpRequest) msg;
+//            System.out.println("messageType:" + request.headers().get("messageType"));
+//            System.out.println("businessType:" + request.headers().get("businessType"));
+//            if (HttpHeaders.isContentLengthSet(request)) {
+//                reader = Unpooled.buffer((int) HttpHeaders.getContentLength(request));
+//            }
+//        }
+//
+//        if (msg instanceof HttpContent) {
+//            HttpContent httpContent = (HttpContent) msg;
+//            ByteBuf content = httpContent.content();
+//            reader.reading(content);
+//            content.release();
+//
+//            if (reader.isEnd()) {
+//                String resultStr = new String(reader.readFull());
+//                System.out.println(resultStr);
+//
+//                FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer("I am ok"
+//                        .getBytes()));
+//                response.headers().set(CONTENT_TYPE, "text/plain");
+//                response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+//                response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+//                ctx.write(response);
+//                ctx.flush();
+//            }
+//        }
+
+
+        FullHttpRequest fullRequest = (FullHttpRequest) msg;
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
 }
+
+
--- java-advanced-project/src/main/java/com/sky/week03nio/NettyServer.java
+++ java-advanced-project/src/main/java/com/sky/week03nio/NettyServer.java
@@ -1,4 +1,40 @@
 package com.sky.week03nio;
 
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
 public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workGoup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+        bootstrap.group(boosGroup,workGoup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                                  @Override
+                                  public void initChannel(SocketChannel ch) throws Exception {
+                                      // 编码
+                                      ch.pipeline().addLast(new HttpResponseEncoder());
+                                      // 解码
+                                      ch.pipeline().addLast(new HttpRequestDecoder());
+                                      ch.pipeline().addLast(new HttpServerInboundHandler());
+                                  }
+                              });
+        ChannelFuture channelFuture = bootstrap.bind(8000).sync();
+        channelFuture.channel().closeFuture().sync();
+
+        workGoup.shutdownGracefully();
+        boosGroup.shutdownGracefully();
+    }
 }
