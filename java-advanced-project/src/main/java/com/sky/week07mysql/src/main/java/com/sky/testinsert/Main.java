88c894dc5c93fcd47c323a4f4296eedee1c3b901
skyming <skyming1126@gmail.com>
2021-06-21 18:05:44 +0800

    fix

--- java-advanced-project/src/main/java/com/sky/week07mysql/src/main/java/com/sky/testinsert/Main.java
+++ java-advanced-project/src/main/java/com/sky/week07mysql/src/main/java/com/sky/testinsert/Main.java
@@ -2,24 +2,33 @@ package com.sky.testinsert;
 
 import com.sky.util.DBUtils;
 import com.sky.util.DBUtils;
+import org.apache.http.client.utils.DateUtils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        final int num = 100;
+        final int num = 1;
         Connection connection = DBUtils.getConnection();
         connection.setAutoCommit(false);
-        String sql = "INSERT INTO  t_order (user_id,sum_price,purchase_price,order_status)\n" +
-                "VALUE(?,?,?,?) ";
+        String sql = "INSERT INTO  t_order (user_id,sum_price,purchase_price,create_time,order_status)\n" +
+                "VALUE(?,?,?,?,?) ";
         PreparedStatement statement = connection.prepareStatement(sql);
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        String datetime = format.format(date);
         for (int i = 0; i < num; i++) {
             statement.setInt(1,1);
             statement.setString(2, String.valueOf(i));
             statement.setString(3, String.valueOf(i*0.9));
-            statement.setString(4, String.valueOf(1));
+            statement.setString(4, datetime);
+            statement.setString(5, String.valueOf(1));
+            statement.addBatch();
         }
 
         long start = System.currentTimeMillis();
